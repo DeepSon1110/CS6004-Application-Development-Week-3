6c7c18c09160f9cf1fe01276a2987d4af3a87e48
Dip_San <khadkasandip489@gmail.com>
2025-11-11 09:54:25 +0545

    This is the task 4 of week 3 workshop

---
+++ Task 4/Program.cs
@@ -0,0 +1,50 @@
+﻿//Task 4 : If else & switch
+
+//creating class Program
+class Program
+{   
+    //main method
+    static void Main()
+    {
+        Console.Write("Enter a number (1-7): ");
+
+        //taking input from the user
+        string input = Console.ReadLine();
+
+        
+        if (int.TryParse(input, out int dayNumber))
+        {
+            switch (dayNumber)
+            {
+                case 1:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 2:
+                    Console.WriteLine("Monday");
+                    break;
+                case 3:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 5:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 6:
+                    Console.WriteLine("Friday");
+                    break;
+                case 7:
+                    Console.WriteLine("Saturday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day number");
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid day number");
+        }
+    }
+}
