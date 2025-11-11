8ac5a9e5325998ac9b2b1b4f16dd1bcb9290734d
Dip_San <khadkasandip489@gmail.com>
2025-11-11 09:49:41 +0545

    This is task 3 of week 3 workshop

---
+++ Task 3/Program.cs
@@ -0,0 +1,33 @@
+﻿//Task 3 : If else and switch
+
+// Creating class Program
+class Program
+{
+    //creating main method
+    static void Main()
+    {
+        Console.Write("Please enter your age: ");
+        string input = Console.ReadLine();
+
+        // Trying to parse the input to an integer safely as input takes string 
+        if (int.TryParse(input, out int age))
+        {
+            if (age < 13)
+            {
+                Console.WriteLine("Child");
+            }
+            else if (age >= 13 && age <= 19)
+            {
+                Console.WriteLine("Teenager");
+            }
+            else 
+            {
+                Console.WriteLine("Adult");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+    }
+}
